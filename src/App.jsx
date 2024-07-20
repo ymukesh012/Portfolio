2b948ab28313d74007a364a5ebe779294c828182
ymukesh012 <ymukesh012@gmail.com>
2024-07-21 01:13:58 +0545

    navsetup

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
 import styles from "./App.module.css";
+import { Navbar } from "./components/Navbar/Navbar";
+
+
 
 function App() {
-  
 
   return (
-    <>
-     <div className={styles.App}>Hello World!</div>
-    </>
+  
+     <div className={styles.App}> 
+<Navbar/>
+     </div>
+    
   )
 }
 
